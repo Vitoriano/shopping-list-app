930e75a1e08cef5a4239b04e1fab71dca206628e
Vitoriano <vitorianoernandes2012@gmail.com>
2025-01-04 11:21:34 -0300

    add initial routes

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,13 @@
+import { ThemedText } from "@/components/ThemedText";
+import { View } from "react-native";
+
+
+export default function SignInScreen() {
+  return (
+    <View>
+      <ThemedText type="title">
+        SignIn
+      </ThemedText>
+    </View>
+  )
+}
