e56bd81e19f939060266da6f648f4a031598f95d
Vitoriano <vitorianoernandes2012@gmail.com>
2025-03-18 10:47:13 -0300

    add new features, icons, pages, presentation

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,6 +1,17 @@
-import { Stack } from "expo-router";
+import Button from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-expo";
+import { Redirect, router, Stack, useRouter } from "expo-router";
 
 export default function HomeRoutesLayout() {
+
+  //TODO: remove this code later
+  const router = useRouter()
+
+  const { user } = useUser();
+
+  if(!user) {
+    return <Redirect  href={'/(auth)'}/>
+  }
 
   return (
     <Stack
@@ -18,6 +29,33 @@ export default function HomeRoutesLayout() {
       }}
     >
       <Stack.Screen name="index" options={{ headerTitle: "Shopping Lists"}}/>
+      <Stack.Screen name="list/new/index" 
+        options={{
+          presentation: 'formSheet',
+          sheetGrabberVisible: true,
+          headerShown: false
+        }}
+      />
+       <Stack.Screen name="profile" 
+        options={{
+          presentation: 'formSheet',
+          sheetAllowedDetents: [0.75, 1],
+          sheetGrabberVisible: true,
+          headerShown: false
+        }}
+      />
+      <Stack.Screen name="list/new/scan" 
+        options={{
+          presentation: 'fullScreenModal',
+          headerLargeTitle: false,
+          headerTitle: "Scan QR code",
+          headerLeft: () => (
+            <Button variant="ghost" onPress={() => router.back()}>
+              Cancel
+            </Button>
+          )
+        }}
+      />
 
     </Stack>
   )
--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,21 +1,47 @@
 import { ThemedText } from "@/components/ThemedText";
 import { BodyScrollView } from "@/components/ui/BodyScrollView";
 import Button from "@/components/ui/button";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import { appleBlue } from "@/constants/Colors";
 import { useClerk } from "@clerk/clerk-expo";
-import { View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { Pressable, View } from "react-native";
 
 export default function HomeScreen() {
 
   const { signOut } = useClerk();
+  const router = useRouter();
+
+  const renderHeaderRight = () => {
+    return (
+      <Pressable onPress={() => router.push('/(home)/list/new')}>
+        <IconSymbol name="plus" color={appleBlue}/>
+      </Pressable>
+    )
+  }
+
+  const renderHeaderLeft = () => {
+    return (
+      <Pressable onPress={() => router.push('/(home)/profile')}>
+        <IconSymbol name="gear" color={appleBlue}/>
+      </Pressable>
+    )
+  }
 
   return (
-    <BodyScrollView
-      contentContainerStyle={{
-        padding: 16
-      }}
-    >
-      <ThemedText type="title"> Home In </ThemedText>
-      <Button onPress={signOut}> Sign Out</Button>
-    </BodyScrollView>
+    <>
+      <BodyScrollView
+        contentContainerStyle={{
+          padding: 16
+        }}
+      >
+        <Stack.Screen options={{
+          headerRight: renderHeaderRight,
+          headerLeft: renderHeaderLeft
+        }}/>
+        <ThemedText type="title"> Home In </ThemedText>
+        <Button onPress={signOut}> Sign Out</Button>
+      </BodyScrollView>
+    </>
   )
 }
