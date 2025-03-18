3281f7cb99dd51d21947a7bfe6d490535529689e
Vitoriano <vitorianoernandes2012@gmail.com>
2025-03-18 11:39:20 -0300

    add + code;

--- app/(home)/list/new/index.tsx
+++ app/(home)/list/new/index.tsx
@@ -1,16 +1,37 @@
+import { IconCircle } from "@/components/IconCircle";
 import { ThemedText } from "@/components/ThemedText";
 import { BodyScrollView } from "@/components/ui/BodyScrollView";
-import { StyleSheet } from 'react-native';
+import { backgroundColors, emojies } from "@/constants/Colors";
+import { useMemo } from "react";
+import { StyleSheet, View } from 'react-native';
 
 export default function NewListScreen() {
+
+  const randomEmoji = useMemo( () => emojies[Math.floor(Math.random() * emojies.length)], [])
+  const randomColor = useMemo( () => backgroundColors[Math.floor(Math.random() * backgroundColors.length)], [])
   return (
     <BodyScrollView contentContainerStyle={{
       padding: 16,
     }}>
-      <ThemedText type="subtitle">Better Together</ThemedText>
-      <ThemedText type="defaultSemiBold" style={styles.subtitle}>
-        Create shared shopping lists and collaborate in real time with family and friends
-      </ThemedText>
+      <View 
+        style={{
+          alignItems: 'center',
+          gap: 16,
+          marginTop: 32
+        }}
+      >
+        <IconCircle emoji={randomEmoji} style={{ 
+          backgroundColor: randomColor,
+          marginBottom: 8
+          }} 
+          size={60}
+        />
+        <ThemedText type="subtitle" style={{ fontSize: 32}}>Better Together</ThemedText>
+        <ThemedText type="defaultSemiBold" style={styles.subtitle}>
+          Create shared shopping lists and collaborate in real time with family and friends
+        </ThemedText>
+      </View>
+     
     </BodyScrollView>
   )
 }
