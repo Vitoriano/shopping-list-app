f11633a88533459d9aa2e8437b9cded80a9b171b
Vitoriano <vitorianoernandes2012@gmail.com>
2025-03-18 11:18:13 -0300

    add assets

--- app/(home)/list/new/create.tsx
+++ app/(home)/list/new/create.tsx
@@ -1,10 +1,22 @@
 import { ThemedText } from "@/components/ThemedText";
 import { BodyScrollView } from "@/components/ui/BodyScrollView";
+import { StyleSheet } from 'react-native';
 
 export default function CreateScreen() {
   return (
-    <BodyScrollView>
-      <ThemedText>Create</ThemedText>
+    <BodyScrollView contentContainerStyle={{
+      padding: 16,
+    }}>
+      <ThemedText type="subtitle">Better Together</ThemedText>
+      <ThemedText type="defaultSemiBold" style={styles.subtitle}>
+        Create shared shopping lists and collaborate in real time with family and friends
+      </ThemedText>
     </BodyScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  subtitle: {
+
+  }
+});
